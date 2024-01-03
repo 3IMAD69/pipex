3b60347a491489408511b175015ea52bbaf8b236
Imad Dhaimy <idhaimy@e1r10p8.1337.ma>
2024-01-03 18:09:49 +0100

    lib

---
+++ libft/ft_lstiter_bonus.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: idhaimy <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/04 11:46:35 by idhaimy           #+#    #+#             */
+/*   Updated: 2023/11/15 16:19:35 by idhaimy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	t_list	*current;
+
+	if (!lst || !f)
+		return ;
+	current = lst;
+	while (current != NULL)
+	{
+		f(current->content);
+		current = current->next;
+	}
+}
---
+++ libft/ft_lstnew_bonus.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: idhaimy <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/03 17:44:49 by idhaimy           #+#    #+#             */
+/*   Updated: 2023/11/15 13:35:03 by idhaimy          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*newnode;
+
+	newnode = (t_list *)malloc(sizeof(t_list));
+	if (newnode)
+	{
+		newnode->content = content;
+		newnode->next = NULL;
+	}
+	return (newnode);
+}
